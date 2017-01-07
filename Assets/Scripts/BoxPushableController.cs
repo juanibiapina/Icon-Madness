2781e945ffc34b2b46702d48dfa8c805f0ded0bc
Juan Ibiapina <juanibiapina@gmail.com>
2017-01-07 21:29:12 -0200

    refactor: Extract TryMove method

--- Assets/Scripts/BoxPushableController.cs
+++ Assets/Scripts/BoxPushableController.cs
@@ -21,108 +21,55 @@ public class BoxPushableController : MonoBehaviour {
 			collisions [collision.gameObject] = normal;
 
 			if (body.velocity.sqrMagnitude > 0) {
-				Vector2 direction = body.velocity;
-
-				foreach (KeyValuePair<GameObject, Vector2> entry in collisions) {
-					if (direction.x > 0 && entry.Value.x < 0) {
-						direction.x = 0;
-						continue;
-					}
-
-					if (direction.x < 0 && entry.Value.x > 0) {
-						direction.x = 0;
-						continue;
-					}
-
-					if (direction.y < 0 && entry.Value.y > 0) {
-						direction.y = 0;
-						continue;
-					}
-
-					if (direction.y > 0 && entry.Value.y < 0) {
-						direction.y = 0;
-						continue;
-					}
-				}
-
-				direction.Normalize ();
-
-				body.velocity = direction * speed;	
-
+				TryMove (body.velocity);
 				return;
 			}
 		}
 
 		if (collision.gameObject.CompareTag("Player")) {
-			Vector2 direction = normal;
-
-			foreach(KeyValuePair<GameObject, Vector2> entry in collisions) {
-				if (normal.x > 0 && entry.Value.x < 0) {
-					direction.x = 0;
-					continue;
-				}
-
-				if (normal.x < 0 && entry.Value.x > 0) {
-					direction.x = 0;
-					continue;
-				}
-
-				if (normal.y < 0 && entry.Value.y > 0) {
-					direction.y = 0;
-					continue;
-				}
-
-				if (normal.y > 0 && entry.Value.y < 0) {
-					direction.y = 0;
-					continue;
-				}
-			}
-
-			direction.Normalize ();
-
-			body.velocity = direction * speed;	
-				
+			TryMove (normal);
 			return;
 		}
 	}
 
+	void TryMove(Vector2 direction) {
+		foreach(KeyValuePair<GameObject, Vector2> entry in collisions) {
+			if (direction.x > 0 && entry.Value.x < 0) {
+				direction.x = 0;
+				continue;
+			}
+
+			if (direction.x < 0 && entry.Value.x > 0) {
+				direction.x = 0;
+				continue;
+			}
+
+			if (direction.y < 0 && entry.Value.y > 0) {
+				direction.y = 0;
+				continue;
+			}
+
+			if (direction.y > 0 && entry.Value.y < 0) {
+				direction.y = 0;
+				continue;
+			}
+		}
+
+		direction.Normalize ();
+
+		body.velocity = direction * speed;	
+	}
+
 	void OnCollisionStay2D(Collision2D collision) {
+		Vector2 normal = collision.contacts [0].normal.normalized;
+
 		if (collision.gameObject.CompareTag("Box Pushable")) {
-			Vector2 normal = collision.contacts [0].normal.normalized;
 			collisions [collision.gameObject] = normal;
 			return;
 		}
 
 		if (collision.gameObject.CompareTag("Player")) {
-			Vector2 normal = collision.contacts [0].normal.normalized;
-			Vector2 direction = normal;
-
-			foreach(KeyValuePair<GameObject, Vector2> entry in collisions) {
-				if (normal.x > 0 && entry.Value.x < 0) {
-					direction.x = 0;
-					continue;
-				}
-
-				if (normal.x < 0 && entry.Value.x > 0) {
-					direction.x = 0;
-					continue;
-				}
-
-				if (normal.y < 0 && entry.Value.y > 0) {
-					direction.y = 0;
-					continue;
-				}
-
-				if (normal.y > 0 && entry.Value.y < 0) {
-					direction.y = 0;
-					continue;
-				}
-			}
-
-			direction.Normalize ();
-
-			body.velocity = direction * speed;	
-
+			TryMove (normal);
 			return;
 		}
 	}
