bfccf3b966d3f8b680f86b83c0cb7e59aa8188ce
Juan Ibiapina <juanibiapina@gmail.com>
2017-01-07 22:53:24 -0200

    feature: Improve Box Pushable object

--- Assets/Scripts/BoxPushableController.cs
+++ Assets/Scripts/BoxPushableController.cs
@@ -3,87 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BoxPushableController : MonoBehaviour {
-
-	public float speed;
 		
 	private Rigidbody2D body;
-	private Dictionary<GameObject, Vector2> collisions;
 
 	void Start () {
-		collisions = new Dictionary<GameObject, Vector2> ();
 		body = GetComponent<Rigidbody2D> ();
 	}
 
 	void OnCollisionEnter2D(Collision2D collision) {
-		Vector2 normal = collision.contacts [0].normal.normalized;
-
-		if (collision.gameObject.CompareTag("Box Pushable")) {
-			collisions [collision.gameObject] = normal;
-
-			if (body.velocity.sqrMagnitude > 0) {
-				TryMove (body.velocity);
-				return;
-			}
-		}
-
 		if (collision.gameObject.CompareTag("Player")) {
-			TryMove (normal);
-			return;
-		}
-	}
-
-	void TryMove(Vector2 direction) {
-		foreach(KeyValuePair<GameObject, Vector2> entry in collisions) {
-			if (direction.x > 0 && entry.Value.x < 0) {
-				direction.x = 0;
-				continue;
-			}
-
-			if (direction.x < 0 && entry.Value.x > 0) {
-				direction.x = 0;
-				continue;
-			}
-
-			if (direction.y < 0 && entry.Value.y > 0) {
-				direction.y = 0;
-				continue;
-			}
-
-			if (direction.y > 0 && entry.Value.y < 0) {
-				direction.y = 0;
-				continue;
-			}
-		}
-
-		direction.Normalize ();
-
-		body.velocity = direction * speed;	
-	}
-
-	void OnCollisionStay2D(Collision2D collision) {
-		Vector2 normal = collision.contacts [0].normal.normalized;
-
-		if (collision.gameObject.CompareTag("Box Pushable")) {
-			collisions [collision.gameObject] = normal;
-			return;
-		}
-
-		if (collision.gameObject.CompareTag("Player")) {
-			TryMove (normal);
-			return;
+			body.constraints = RigidbodyConstraints2D.FreezeRotation;
 		}
 	}
 
 	void OnCollisionExit2D(Collision2D collision) {
-		if (collision.gameObject.CompareTag ("Box Pushable")) {
-			collisions.Remove (collision.gameObject);
-
-			return;
-		}
-
 		if (collision.gameObject.CompareTag("Player")) {
+			body.constraints = RigidbodyConstraints2D.FreezeAll;
 			body.velocity = Vector2.zero;
-			return;
 		}
 	}
 }
